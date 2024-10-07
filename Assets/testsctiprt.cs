31afd7d154a8ab33693141ec9fcdcfb1ac3e3432
motaz98 <motazsherif126@gmail.com>
2024-10-07 20:00:28 +0300

    Convert the next Scenes to VR ("Splash Screen - Menu List")

---
+++ Assets/testsctiprt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testsctiprt : MonoBehaviour
+{
+    public void Test()
+    {
+        print("Test");
+    }
+}
